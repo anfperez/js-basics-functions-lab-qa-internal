31305a20ec2998f9f5ac8f2cea7ca3864158cf29
Angela Perez <angela.perez@flatironschool.com>
2018-08-04 02:51:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function calculatesFarePrice (start, end) {
   const distance = distanceTravelledInFeet(start, end);
 
   if (distance <= 400) {
-    return 0;
+    return "$0";
   } else if (distance > 400 && distance <= 2000) {
     return .02 * (distance - 400);
   } else if (distance > 2000 && distance < 2500) {
