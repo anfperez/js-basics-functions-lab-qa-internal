37a3b8f66e0162319f8d656ebedcf979dedf2fab
Angela Perez <angela.perez@flatironschool.com>
2018-08-04 02:24:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+/* here's my solution using absolute value 
+function distanceFromHqInBlocks(location) {
+  let numBlocks = 42 - location;
+  return Math.abs(location);
+}
+
+distanceFromHqInBlocks(43);
+
+distanceFromHqInBlocks(50);
+
+distanceFromHqInBlocks(34);
+*/
+
+function distanceFromHqInBlocks (blockNumber) {
+  if (blockNumber > 42) {
+    return blockNumber - 42;
+  } else {
+    return 42 - blockNumber;
+  }
+}
+
+function distanceFromHqInFeet(blockNumber) {
+  return distanceFromHqInBlocks(blockNumber) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  let startBlock = start * 264;
+  let endBlock = end * 264;
+  return Math.abs(startBlock - endBlock);
+}
+
+
